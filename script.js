5ddac4fb0570aadba5ed81a5e84949310a231eb7
Jephthah2 <225693441+Jephthah2@users.noreply.github.com>
2025-10-31 07:35:20 +0000

    add playRound function and playGame function

--- script.js
+++ script.js
@@ -13,4 +13,43 @@ function getHumanChoice() {
   return choice;
 }
 
-console.log(getHumanChoice())
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+  humanChoice = humanChoice.toLowerCase();
+
+  if (humanChoice === computerChoice) {
+    console.log(`It is a draw.`)
+  } else if ((humanChoice === 'rock' && computerChoice === 'scissors') || (humanChoice === 'scissors' && computerChoice === 'paper') || (humanChoice ===  'paper' && computerChoice === 'rock')) {
+    humanScore++;
+    console.log(`You win! ${humanChoice} beats ${computerChoice}`)
+  } else {
+    computerScore++;
+    console.log(`You lose! ${computerChoice} beats ${humanChoice}`)
+  }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+
+function playGame() {
+  for (let i = 0; i < 5; i++) {
+    console.log(`Round ${i + 1}`);
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+    playRound(humanSelection, computerSelection)
+  } 
+  
+  if (humanScore > computerScore) {
+    console.log(`You win the game`);
+  } else if (computerScore > humanScore) {
+    console.log(`You lose the game`);
+  } else {
+    console.log(`It is a draw`);
+  }
+}
+
+playGame();
